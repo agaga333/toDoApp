6e3681c19e8ffd38ef5e716e741f34b2119b5018
agaga333 <ajankowska0404@gmail.com>
2016-10-26 20:03:39 +0200

    ToDoList

---
+++ jstodo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Agnieszka on 2016-10-26.
+ */
+var input = document.getElementById("input-text"),
+    addBtn = document.getElementById("add-btn"),
+    list = document.getElementById("to-do-list");
