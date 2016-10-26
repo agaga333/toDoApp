f1b5008bb5351481704d6997d2c7fd79ff5f5dc7
agaga333 <ajankowska0404@gmail.com>
2016-10-26 22:14:08 +0200

    ToDoList

--- jstodo.js
+++ jstodo.js
@@ -5,10 +5,13 @@ var input = document.getElementById("input-text"),
     addBtn = document.getElementById("add-btn"),
     list = document.getElementById("list-box");
 
-var todos = [ {text: "Clean the toilet"}, {text: "Wash the dishes"}, {text: "Sweep the floor"} ];
+var todos = [ ];
 
 function displayToDos() {
     var todosList = '<ul>';
+    if (todos.length === 0) {
+        return;
+    }
     for (var i = 0, len = todos.length; i < len; i++) {
         todosList += '<li>' + todos[i].text + '</li>';
     }
@@ -16,16 +19,26 @@ function displayToDos() {
     list.innerHTML = todosList;
 }
 
-displayToDos();
-
 function addToDo() {
     todos.push({text: input.value});
     displayToDos();
+    input.value = '';
 }
 
 addBtn.addEventListener("click", addToDo);
 
+function validateAddBtn() {
+    if (this.value.length === 0) {
+        addBtn.disabled = true;
+    } else {
+        addBtn.disabled = false;
+    }
+}
+
+input.addEventListener("keypress", validateAddBtn);
+
 function init() {
+    validateAddBtn();
     displayToDos();
 }
 
