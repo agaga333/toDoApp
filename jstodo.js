ecdbd57bcc047906c0a34754a43714d6663366b5
agaga333 <ajankowska0404@gmail.com>
2016-10-26 20:10:47 +0200

    ToDoList

--- jstodo.js
+++ jstodo.js
@@ -4,3 +4,9 @@
 var input = document.getElementById("input-text"),
     addBtn = document.getElementById("add-btn"),
     list = document.getElementById("to-do-list");
+
+var clean = { text: "Clean the toilet"};
+var wash = { text: "Wash the dishes"};
+var sweep = { text: "Sweep the floor"};
+
+var todos = [ clean, wash, sweep ];
