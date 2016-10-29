47da1c7583c672d9053a497aac30bdc8463adf2d
agaga333 <ajankowska0404@gmail.com>
2016-10-29 16:21:52 +0200

    ToDoList

--- chartexercise.js
+++ chartexercise.js
@@ -1,3 +1,29 @@
 /**
  * Created by Agnieszka on 2016-10-29.
  */
+var barChartData = {
+    labels : ["January","February","March","April","May","June","July"],
+    datasets : [
+        {
+            fillColor : "#0088ff",
+            strokeColor : "rgba(0,0,0,0)",
+            highlightFill: "#0070ff",
+            data: [10,50,40,30,20,10,80]
+        }
+    ]
+}
+var opt = {
+    responsive:true,
+    animation: false,
+    showScale: true,
+    scaleShowGridLines : false,
+    barShowStroke: false,
+    tooltipXPadding: 10,
+    tooltipYPadding: 6,
+    tooltipFontSize: 18,
+    tooltipFontStyle: 'bold',
+};
+
+var ctx = document.getElementById("canvas").getContext("2d");
+window.myBar = new Chart(ctx).Bar(barChartData, opt);
+
