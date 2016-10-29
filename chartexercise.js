4e49acefd9b872d5086d0bfd273fbd2c1b13046a
agaga333 <ajankowska0404@gmail.com>
2016-10-29 17:00:15 +0200

    ToDoList

--- chartexercise.js
+++ chartexercise.js
@@ -1,14 +1,27 @@
 /**
  * Created by Agnieszka on 2016-10-29.
  */
+function findCountryName(country) {
+    return country = country.name
+}
+
+var countriesNames = countries.map(findCountryName);
+
+function findCountryPopulation(country) {
+    return country = country.population
+}
+
+var countriesNPopulations = countries.map(findCountryPopulation);
+
+
 var barChartData = {
-    labels : ["January","February","March","April","May","June","July"],
+    labels : countriesNames,
     datasets : [
         {
             fillColor : "#0088ff",
             strokeColor : "rgba(0,0,0,0)",
             highlightFill: "#0070ff",
-            data: [10,50,40,30,20,10,80]
+            data: countriesNPopulations
         }
     ]
 };
